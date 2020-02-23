31e84ac5e915369afeacff6a9f585e5a3dc954c7
mlarese <mauro.larese@gmail.com>
2020-02-23 09:53:46 +0100

    4th commit

--- components/Profile/profileReportForm.vue
+++ components/Profile/profileReportForm.vue
@@ -8,32 +8,31 @@
         </div>
         <v-form ref="form" lazy-validation>
             <v-layout row wrap>
+                <v-flex xs12 sm6 md6>
                 <v-autocomplete
-                        color="deep-purple"
+                        color="green"
                         label="Brand Select"
                 ></v-autocomplete>
-                <v-text-field label="Link Web" hide-details v-model="brand_name" type="text" />
-                <v-text-field label="Title of Report" hide-details v-model="report_title" type="text" />
-                <v-textarea
-                        v-model="bio"
-                        auto-grow
-                        box
-                        color="deep-purple"
-                        label="Bio"
-                        rows="1"
-                ></v-textarea>
-                <v-file-input show-size counter multiple label="File input"></v-file-input>
-                <v-text-field label="IP Address" hide-details type="number" />
-                <v-autocomplete
-                        color="deep-purple"
-                        label="User Agent"
-                ></v-autocomplete>
+                </v-flex>
+                <v-flex xs12 sm6 md6><v-text-field label="Link Web" hide-details color="green" v-model="brand_name" type="text" /></v-flex>
+                <v-flex xs12 sm6 md6> <v-text-field label="Title of Report" color="green" hide-details v-model="report_title" type="text" /></v-flex>
+                <v-flex xs12 sm6 md6><v-text-field label="IP Address" color="green" hide-details type="number" /></v-flex>
+                <v-flex xs12 sm12 md12><v-textarea auto-grow box color="green" label="Bio"></v-textarea></v-flex>
+                <v-flex xs12 sm12 md12><vue-upload-multiple-image
+                        @upload-success="uploadImageSuccess"
+                        @before-remove="beforeRemove"
+                        @edit-image="editImage"
+                        @data-change="dataChange"
+                        :data-images="images"
+                        label="images"
+                ></vue-upload-multiple-image></v-flex>
+                <v-flex xs12 sm6 md6><v-autocomplete color="green" label="User Agent"></v-autocomplete></v-flex>
 
             </v-layout>
 
             <v-layout row wrap>
                 <v-flex xs2 offset-xs5>
-                    <v-btn  style="width:100%"  color="primary"  @click="onAdd">
+                    <v-btn  style="width:100%"  color="primary"  @click="save">
                         {{$vuetify.t('Save') }}
                     </v-btn>
                 </v-flex>
@@ -46,30 +45,32 @@
     import {mapState, mapActions} from 'vuex'
     import FormPanel from '../General/FormPanel'
     import GridButton from '../General/GridButton'
-
+    import FileUpload from 'vue-upload-component'
+    import VueUploadMultipleImage from 'vue-upload-multiple-image'
     export default {
         components: {
-            FormPanel, GridButton
+            FormPanel, GridButton,FileUpload,VueUploadMultipleImage
         },
-        data () {
-            return {}
+        data() {
+            return {
+                images: []
+            }
         },
         computed: {
             ...mapState('profileReports', ['$record']),
             //isValid () {
-              //  if(!this.$record.brand_name) return false
-              //  if(!this.$record.sms_mt_text_message) return false
-              //  if(!this.$record.conversion_grace_period) return false
-              //  return true
-           // }
+            //  if(!this.$record.brand_name) return false
+            //  if(!this.$record.sms_mt_text_message) return false
+            //  if(!this.$record.conversion_grace_period) return false
+            //  return true
+            // }
         },
         methods: {
-            onAdd () {
+            onAdd() {
                 this.save()
                     .then(r => this.$router.go(-1))
             },
-            ...mapActions('profileReports', ['add', 'save']),
-
+            ...mapActions('profileReports', ['add', 'save'])
         }
     }
 </script>
---
+++ pages/tests.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-layout>
+    <profile-report-form/>
+  </v-layout>
+</template>
+<script>
+    import profileReportForm from '../components/Profile/profileReportForm'
+    export default {
+        components : {profileReportForm}
+    }
+</script>
