802bada6dbf0d9fc3c858e9abe2bc82cd7c9c5d0
mlarese <mauro.larese@gmail.com>
2020-03-18 15:36:08 +0100

    brands

--- components/Profile/profilereportlist.vue
+++ components/Profile/profilereportlist.vue
@@ -40,12 +40,7 @@
                 </td>
                 <td>{{ item.ip_address }}</td>
                 <td>{{ item.user_agent }}</td>
-                <!--<td width="1" class="pa-1">
-                    <GridButton icon="edit" color="primary" @click="onEdit(item.id )"></GridButton>
-                </td>
-                <td width="1" class="pa-1">
-                    <GridButton icon="delete" color="error" @click="onDelete(item.id)"></GridButton>
-                </td>-->
+
             </template>
             <template slot="pageText" slot-scope="{ pageStart, pageStop, itemsLength }">
                 {{$vuetify.t('From')}} {{ pageStart }} {{$vuetify.t('To')}} {{ pageStop }}  {{$vuetify.t('of')}} {{ itemsLength }}
